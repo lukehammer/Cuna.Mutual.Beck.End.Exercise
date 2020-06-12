2de65e1590546ecb334c059a044ffe8ca14ccf86
luke hammer <luke@lukehammermagic.com>
2020-06-12 15:24:02 -0700

    Created models

---
+++ Cuna.Mutual.Back.End.Exercise/Models/MacGuffin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Cuna.Mutual.Back.End.Exercise.Api.Controllers;
+
+namespace Cuna.Mutual.Back.End.Exercise.Api.Models
+{
+    public class MacGuffin
+    {
+        protected MacGuffin()
+        {
+            //needed for ef 
+        }
+
+        public MacGuffin(string body)
+        {
+            Body = body;
+        }
+
+        public string Body { get; protected set;  }
+        
+        [Key]
+        public int Id { get; protected set;  }
+
+
+        public virtual List<Status> Statuses{ get; protected set; } = new List<Status>();
+
+        public void UpdateStatus(Status status)
+        {
+            Statuses.Add(status);
+        }
+
+    }
+}
---
+++ Cuna.Mutual.Back.End.Exercise/Models/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cuna.Mutual.Back.End.Exercise.Api.Models
+{
+    public class Status
+    {
+        [Key]
+        public int Id { get; protected set; }
+        public DateTimeOffset Time { get; protected set; } = DateTimeOffset.Now;
+        public string State { get; set; }
+        public string Detail { get; set;  }
+
+    }
+}
